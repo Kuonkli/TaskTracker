f9bed03c2b6d9fbc0119cb39a894d86f69356157
Kuonkli <korotkih1307@mail.ru>
2026-02-13 21:15:43 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',  // ← берем из .env
     headers: {
         'Content-Type': 'application/json',
     },
@@ -21,5 +21,6 @@ api.interceptors.response.use(
         return Promise.reject(error);
     }
 );
+
 
 export default api;
